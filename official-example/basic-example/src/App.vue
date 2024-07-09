3a2b4d5fe5c71ab0c745a09f158776ea6329aec7
rugii913 <rugii913@gmail.com>
2024-07-10 00:54:19 +0900

    add: 간소화된 라우팅 임시 작성
    간소화된 라우팅 임시 작성

--- official-example/basic-example/src/App.vue
+++ official-example/basic-example/src/App.vue
@@ -1,7 +1,43 @@
-<script setup>
+<script>
+/* 
+- 간소화된 라우팅 임시 작성
+  - (참고) https://v3-docs.vuejs-korea.org/guide/scaling-up/routing.html
+- 쓸만한 라우팅은 vue-router 라이브러리 사용할 것
+  - (참고) https://router.vuejs.kr/
+*/
 import VueAutoExampleMain from "./components/vue-auto-example/VueAutoExampleMain.vue"
+import HomePage from "./pages/HomePage.vue"
+
+const routes = {
+  "/": HomePage,
+  "/vue-auto-example": VueAutoExampleMain,
+}
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || "/"]
+    }
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash
+    })
+  }
+}
 </script>
 
 <template>
-  <VueAutoExampleMain />
+  <div>
+    <a href="#/">Home</a> | 
+    <a href="#/vue-auto-example">Vue.js auto configured example</a>
+  </div>
+  <div>
+    <component :is="currentView" />
+  </div>
 </template>
---
+++ official-example/basic-example/src/pages/HomePage.vue
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<template>
+  <p>hi</p>
+</template>
